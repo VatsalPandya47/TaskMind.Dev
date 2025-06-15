d4dd7e448de95656743e6362243db2f4b627545b
GurvinderSingh <gurvin.singh007@gmail.com>
2025-06-15 15:39:34 -0500

    Test

--- src/components/DashboardTab.tsx
+++ src/components/DashboardTab.tsx
@@ -16,7 +16,9 @@ const DashboardTab = () => {
   if (meetingsLoading || tasksLoading) {
     return (
       <div className="flex items-center justify-center h-64">
-        <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-blue-600"></div>
+        <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-blue-600">
+          Test
+        </div>
       </div>
     );
   }
