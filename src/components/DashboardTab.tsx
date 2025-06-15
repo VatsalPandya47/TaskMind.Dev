11859aa2d0308dbcbe37a4f764fb383729ee268d
GurvinderSingh <gurvin.singh007@gmail.com>
2025-06-15 16:28:31 -0500

    test

--- src/components/DashboardTab.tsx
+++ src/components/DashboardTab.tsx
@@ -17,7 +17,7 @@ const DashboardTab = () => {
     return (
       <div className="flex items-center justify-center h-64">
         <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-blue-600">
-          Test
+          Test 2
         </div>
       </div>
     );
