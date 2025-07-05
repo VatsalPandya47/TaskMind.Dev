00200a5d7ddbd67d5147a511bd4da7f418e8f72e
Joey2247 <qiuyi2247@gmail.com>
2025-07-04 23:04:49 -0700

    optimizationforsmallscreen

--- src/main.tsx
+++ src/main.tsx
@@ -3,3 +3,12 @@ import App from './App.tsx'
 import './index.css'
 
 createRoot(document.getElementById("root")!).render(<App />);
+
+window.addEventListener('resize', () => {
+  const minWidth = 320; // Minimum width for the entire page
+  if (window.innerWidth < minWidth) {
+    document.body.style.width = `${minWidth}px`;
+  } else {
+    document.body.style.width = 'auto';
+  }
+});
